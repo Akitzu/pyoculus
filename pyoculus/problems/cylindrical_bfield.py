bc94b06d989deb124cc6053222236380f595b2ed
Akitzu <ludovic.rais@epfl.ch>
2024-04-02 13:20:35 +0200

    adding the find_axis class method

--- pyoculus/problems/cylindrical_bfield.py
+++ pyoculus/problems/cylindrical_bfield.py
@@ -5,7 +5,9 @@
 
 from .cylindrical_problem import CylindricalProblem
 from .bfield_problem import BfieldProblem
+from ..solvers.fixed_point import FixedPoint
 import numpy as np
+import copy
 
 ## Class that used to setup the cylindrical bfield problem used in ODE solver.
 #
@@ -20,6 +22,24 @@ class CylindricalBfield(CylindricalProblem, BfieldProblem):
         """
 
         super().__init__(R0, Z0, Nfp)
+
+    @classmethod
+    def find_axis(cls, guess, Nfp = 1, **kwargs):
+        """! Find the magnetic axis
+        @param guess the initial guess of the axis
+        @param Nfp the number of field periods
+        @param **kwargs extra parameters for the FixedPoint.find_axis method
+        @returns the axis R0 and Z0
+        """
+        
+        tmpProblem = copy.deepcopy(cls)
+        tmpProblem._R0 = guess[0]
+        tmpProblem._Z0 = guess[1]
+        tmpProblem.Nfp = Nfp
+
+        R0, _, Z0 = FixedPoint.find_axis(tmpProblem, guess, **kwargs)
+        
+        return R0, Z0        
 
     def f_RZ(self, phi, RZ, *args):
         """! Returns ODE RHS
@@ -41,62 +61,34 @@ class CylindricalBfield(CylindricalProblem, BfieldProblem):
 
         return np.array([dRdt, dZdt])
 
-    # def f_RZ_tangent(self, phi, RZ, *args):
-    #     """! Returns ODE RHS, with tangent
-    #     @param zeta cylindrical angle in ODE
-    #     @param RZ \f$(R, Z, dR_1, dZ_1, dR_2, dZ_2)\f$ in ODE
-    #     @param *args extra parameters for the ODE
-    #     @returns the RHS of the ODE, with tangent
-    #     """
-    #     R = RZ[0]
-    #     Z = RZ[1]
-    #     cosphi = np.cos(phi)
-    #     sinphi = np.sin(phi)
+    def f_RZ_tangent(self, phi, RZ, *args):
+        """! Returns ODE RHS, with tangent
+        @param zeta cylindrical angle in ODE
+        @param RZ \f$(R, Z, dR_1, dZ_1, dR_2, dZ_2)\f$ in ODE
+        @param *args extra parameters for the ODE
+        @returns the RHS of the ODE, with tangent
+        """
+        R = RZ[0]
+        Z = RZ[1]
 
-    #     dRZ = np.array([[RZ[2], RZ[4]], [RZ[3], RZ[5]]], dtype=np.float64)
-    #     M = np.zeros([2, 2], dtype=np.float64)
+        dRZ = np.array([[RZ[2], RZ[4]], [RZ[3], RZ[5]]], dtype=np.float64)
+        M = np.zeros([2, 2], dtype=np.float64)
 
-    #     xyz = np.array([
-    #         R * cosphi,
-    #         R * sinphi,
-    #         Z
-    #     ])
+        rphiz = np.array([R, phi, Z])
 
-    #     B = np.array([self.B(xyz, *args)]).T
-    #     Bx = B[0,0]
-    #     By = B[1,0]
-    #     dBdX = np.array(self.dBdX(xyz, *args))
+        B = np.array([self.B(rphiz, *args)]).T
+        dBdRphiZ = np.array(self.dBdX(rphiz, *args))
 
-    #     invJacobian = self._inv_Jacobian(R,phi,Z)
-    #     Jacobian = np.linalg.inv(invJacobian)
+        dRphiZ = B.T[0]
 
-    #     dRphiZ = np.matmul(invJacobian, B).T[0]
+        dRdt = dRphiZ[0]/dRphiZ[1]
+        dZdt = dRphiZ[2]/dRphiZ[1]
 
-    #     dRdt = dRphiZ[0]/dRphiZ[1]
-    #     dZdt = dRphiZ[2]/dRphiZ[1]
+        M[0,0] = dBdRphiZ[0,0] / dRphiZ[1]  - dRphiZ[0] / dRphiZ[1]**2 * dBdRphiZ[1,0]
+        M[0,1] = dBdRphiZ[0,2] / dRphiZ[1]  - dRphiZ[0] / dRphiZ[1]**2 * dBdRphiZ[1,2]
+        M[1,0] = dBdRphiZ[2,0] / dRphiZ[1]  - dRphiZ[2] / dRphiZ[1]**2 * dBdRphiZ[1,0]
+        M[1,1] = dBdRphiZ[2,2] / dRphiZ[1]  - dRphiZ[2] / dRphiZ[1]**2 * dBdRphiZ[1,2]
 
-    #     # convert from @(Bx,By,Bz)/@(x,y,z) to @(B^R,B^phi,B^Z)/@(R,phi,Z)
-    #     dBdRphiZ = np.matmul(np.matmul(invJacobian, dBdX), Jacobian)
-    #     dinvJ = np.array([
-    #         [0, -Bx * sinphi + By * cosphi, 0 ],
-    #         [Bx * sinphi / R**2 - By * cosphi / R**2, -Bx * cosphi / R - By * sinphi / R, 0],
-    #         [0, 0, 0]
-    #     ])
-    #     dBdRphiZ = dBdRphiZ + dinvJ
+        dRZ = np.matmul(M, dRZ)
 
-    #     M[0,0] = dBdRphiZ[0,0] / dRphiZ[1]  - dRphiZ[0] / dRphiZ[1]**2 * dBdRphiZ[1,0]
-    #     M[0,1] = dBdRphiZ[0,2] / dRphiZ[1]  - dRphiZ[0] / dRphiZ[1]**2 * dBdRphiZ[1,2]
-    #     M[1,0] = dBdRphiZ[2,0] / dRphiZ[1]  - dRphiZ[2] / dRphiZ[1]**2 * dBdRphiZ[1,0]
-    #     M[1,1] = dBdRphiZ[2,2] / dRphiZ[1]  - dRphiZ[2] / dRphiZ[1]**2 * dBdRphiZ[1,2]
-
-    #     dRZ = np.matmul(M, dRZ)
-
-    #     return np.array([dRdt, dZdt, dRZ[0, 0], dRZ[1, 0], dRZ[0, 1], dRZ[1, 1]])
-
-    # @staticmethod
-    # def _inv_Jacobian(R, phi, Z):
-    #     return np.array([
-    #         [np.cos(phi), np.sin(phi), 0],
-    #         [-np.sin(phi)/R, np.cos(phi)/R, 0],
-    #         [0,0,1]
-    #         ])
+        return np.array([dRdt, dZdt, dRZ[0, 0], dRZ[1, 0], dRZ[0, 1], dRZ[1, 1]])
