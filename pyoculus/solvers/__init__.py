0914252fc33343ce1b31d3f1c3f11fa771a4a417
Akitzu <ludovic.rais@epfl.ch>
2024-04-05 18:02:35 +0200

    adding manifold to init

--- pyoculus/solvers/__init__.py
+++ pyoculus/solvers/__init__.py
@@ -1,5 +1,6 @@
 from .poincare_plot import PoincarePlot
 from .fixed_point import FixedPoint
+from .manifold import Manifold
 from .lyapunov_exponent import LyapunovExponent
 from .flux_surface_gr import FluxSurfaceGR
 from .base_solver import BaseSolver
