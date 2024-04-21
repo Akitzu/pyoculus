3627fbb301cc219b7280ef88a0266d13547d460c
Akitzu <ludovic.rais@epfl.ch>
2024-04-21 14:22:30 +0200

    finding homoclinic points

--- pyoculus/solvers/manifold.py
+++ pyoculus/solvers/manifold.py
@@ -1,7 +1,7 @@
 from ..problems.bfield_problem import BfieldProblem
 from .base_solver import BaseSolver
 from .fixed_point import FixedPoint
-from scipy.optimize import newton_krylov
+from scipy.optimize import newton_krylov, fsolve
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -15,22 +15,9 @@ class Manifold(BaseSolver):
             raise AssertionError("Need a successful fixed point to compute the manifold")
         
         self.fixedpoint = fixedpoint
-
-        # Compute the eigenvalues and eigenvectors of the fixed point
-        eigRes = np.linalg.eig(fixedpoint.jacobian)
-        eigenvalues = eigRes[0]
-
-        # Eigenvectors are stored as columns of the matrix eigRes[1], transposing it to access them as np.array[i]
-        eigenvectors = eigRes[1].T
-        s_index, u_index = 0, 1
-        if eigenvalues[0].real > eigenvalues[1].real:
-            s_index, u_index = 1, 0
-
-        self.vector_u = eigenvectors[u_index]
-        self.lambda_u = eigenvalues[u_index]
-        self.vector_s = eigenvectors[s_index]
-        self.lambda_s = eigenvalues[s_index]
-
+        self.fixedpoint.compute_all_jacobians()
+        
+        # Initialize the manifolds for later computation
         self.unstable = {'+': None, '-': None}
         self.stable = {'+': None, '-': None}
 
@@ -44,7 +31,7 @@ class Manifold(BaseSolver):
         integrator_params["ode"] = bfield.f_RZ
         default_params = {"zeta": 0}
         default_params.update(params)
-
+        
         super().__init__(
             problem=bfield,
             params=default_params,
@@ -52,45 +39,190 @@ class Manifold(BaseSolver):
             integrator_params=integrator_params,
         )
 
-    def compute(self, epsilon = None, **kwargs):
+    @staticmethod
+    def eig(jacobian):
+        """Compute the eigenvalues and eigenvectors of the jacobian and returns them in the order : stable, unstable."""
+        eigRes = np.linalg.eig(jacobian)
+        eigenvalues = eigRes[0]
+
+        # Eigenvectors are stored as columns of the matrix eigRes[1], transposing it to access them as np.array[i]
+        eigenvectors = eigRes[1].T
+        s_index, u_index = 0, 1
+        if eigenvalues[0].real > eigenvalues[1].real:
+            s_index, u_index = 1, 0
+
+        return eigenvalues[s_index], eigenvectors[s_index], \
+               eigenvalues[u_index], eigenvectors[u_index] 
+
+    def choose(self):
+        """Choose the fixed points and their stable or unstable directions."""
+
+        # For now just take twice the same fp
+        self.rfp_s = np.array([self.fixedpoint.x[0], self.fixedpoint.z[0]])
+        self.rfp_u = np.array([self.fixedpoint.x[0], self.fixedpoint.z[0]])
+        self.lambda_s, self.vector_s, self.lambda_u, self.vector_u = self.eig(self.fixedpoint.all_jacobians[0])
+        self.directions = "u+s+"
+
+    def find_N(self, guess_eps_s = 1e-3, guess_eps_u = 1e-3):
+        """Finding the number of times the map needs to be applied for the stable and unstable points to cross."""
+        
+        r_s = self.rfp_s + guess_eps_s * self.vector_s
+        r_u = self.rfp_u + guess_eps_u * self.vector_u
+        
+        first_dir = r_u - r_s
+        last_norm = np.linalg.norm(first_dir)
+
+        n_s, n_u = 0, 0
+        success, stable_evol = False, True
+        while not success:
+            if stable_evol:
+                r_s = self.integrate_single(r_s, 1, -1, ret_jacobian=False)
+                n_s += 1
+            else:
+                r_u = self.integrate_single(r_u, 1, 1, ret_jacobian=False)
+                n_u += 1
+            stable_evol = not stable_evol
+
+            norm = np.linalg.norm(r_u - r_s)
+            #sprint(f"{np.dot(first_dir, r_u - r_s)} / {last_norm} / {norm}")
+            if np.sign(np.dot(first_dir, r_u - r_s)) < 0 and last_norm < norm:
+                success = True
+            last_norm = norm
+
+        if not success:
+            raise ValueError("Could not find N")    
+        else:
+            return n_s, n_u
+    
+    def find_homoclinic(self, guess_eps_s = 1e-3, guess_eps_u = 1e-3, **kwargs):
+        defaults = {"maxiter": 100, "N": None, "N_return": None}
+        defaults.update({key: value for key, value in kwargs.items() if key in defaults})
+
+        if defaults['N'] is None:
+            n_s, n_u = self.find_N(guess_eps_s, guess_eps_u)
+            print(f"Found [n_s, n_u] : [{n_s}, {n_u}]")
+        else:
+            n_s, n_u = defaults['N'], defaults['N']
+
+        if defaults['N_return'] is None:
+            defaults['N_return'] = max(n_s, n_u)
+
+        def evolution(eps):
+            eps_s, eps_u = eps
+            r_s = self.rfp_s + eps_s * self.vector_s
+            r_u = self.rfp_u + eps_u * self.vector_u
+
+            r_s_evolved, jac_s = self.integrate_single(r_s, n_s, -1)
+            r_u_evolved, jac_u = self.integrate_single(r_u, n_u, 1)
+            
+            return r_s_evolved - r_u_evolved, jac_s - jac_u
+        
+        def residual(eps, _):
+            return evolution(eps)[0]
+
+        def jacobian(eps, _):
+            return evolution(eps)[1]
+        
+        eps_s_1, eps_u_1 = fsolve(residual, [guess_eps_s, guess_eps_u], jacobian)
+        if eps_s_1 < 0 or eps_u_1 < 0:
+            raise ValueError("First homoclinic point epsilon cannot be negative.")
+        print(f"Eps 1: {eps_s_1}, {eps_u_1}")
+
+        # Taking the a new guess for the second homoclinic point
+        guess_2 = [eps_s_1*np.sqrt(self.lambda_s), eps_u_1*np.sqrt(self.lambda_u)]
+        n_s, n_u = self.find_N(*guess_2)
+        print(f"Found [n_s, n_u] : [{n_s}, {n_u}]")
+        # # Taking the a new guess for the second homoclinic point
+        # guess_2 = []
+        # rz_tmp = self.integrate(self.rfp_s + eps_s_1 * self.vector_s, 1, -1)
+        # guess_2.append((eps_s_1+np.linalg.norm(rz_tmp[:,1]-rz_tmp[:,0])/2))
+        
+        # rz_tmp = self.integrate(self.rfp_u + eps_u_1 * self.vector_u, 1, 1)
+        # guess_2.append((eps_u_1+np.linalg.norm(rz_tmp[:,1]-rz_tmp[:,0])/2))
+        
+        print(f"Guess 2: {guess_2}")
+
+        eps_s_2, eps_u_2 = fsolve(residual, guess_2, jacobian)
+        if eps_s_2 < 0 or eps_u_2 < 0:
+            raise ValueError("Second homoclinic point epsilon cannot be negative.")
+        print(f"Eps 2: {eps_s_2}, {eps_u_2}")
+
+        # # guess 3 ?
+        # guess_3 = [eps_s_2*np.sqrt(self.lambda_s), eps_u_2*np.sqrt(self.lambda_u)]
+        # print(f"Guess 3: {guess_3}")
+        # eps_s_3, eps_u_3 = fsolve(residual, guess_3, jacobian)
+        # if eps_s_3 < 0 or eps_u_3 < 0:
+        #     raise ValueError("Third homoclinic point epsilon cannot be negative.")
+        # print(f"Eps 3: {eps_s_3}, {eps_u_3}")
+        
+        ret = ( self.integrate(self.rfp_s + eps_s_1 * self.vector_s, defaults['N_return'], -1), self.integrate(self.rfp_u + eps_u_1 * self.vector_u, defaults['N_return'], 1),
+                self.integrate(self.rfp_s + eps_s_2 * self.vector_s, defaults['N_return'], -1), self.integrate(self.rfp_u + eps_u_2 * self.vector_u, defaults['N_return'], 1) )
+        return ret
+
+    def compute(self, epsilon = None, fp_num = None, **kwargs):
+        """Compute the manifolds of the fixed point. If no fixed point number is given, the two manifold coicinding with the tangle plot is computed otherwise 
+        just select the fixed point and compute the manifold in directions."""
         options = {
-            "eps_guess": 2e-6,
+            "eps_guess_s": 2e-6,
+            "eps_guess_u": 2e-6,
             "nintersect": 10,
             "neps": 2,
-            "directions": "u+u-s+s-"
+            "directions": "u+u-s+s-",
+            "eps_s": None,
+            "eps_u": None,
         }
         options.update({key: value for key, value in kwargs.items() if key in options})
         
-        rz_fixedpoint = np.array([self.fixedpoint.x[0], self.fixedpoint.z[0]])
+        if fp_num is not None:
+            if fp_num not in range(self.fixedpoint.qq):
+                raise ValueError("Invalid fixed point number")
+            lambda_s, vector_s, lambda_u, vector_u = self.eig(self.fixedpoint.all_jacobians[fp_num])
+            rfp_u = np.array([self.fixedpoint.x[fp_num], self.fixedpoint.z[fp_num]])
+            rfp_s = np.array([self.fixedpoint.x[fp_num], self.fixedpoint.z[fp_num]])
+        else:
+            options['directions'] = self.directions
+            lambda_s, vector_s = self.lambda_s, self.vector_s
+            lambda_u, vector_u = self.lambda_u, self.vector_u
+            rfp_s, rfp_u = self.rfp_s, self.rfp_u
 
-        if epsilon is None:
-            epsilon = self.find_epsilon(options['eps_guess'], self.vector_u)
-        
-        RZs = self.start_config(epsilon, self.vector_u, self.lambda_u, options['neps'])[0]
+        if epsilon is not None:
+            if options['eps_s'] is not None:
+                print("Warning: both eps_s and epsilon are given, ignoring the eps_s.")
+            if options['eps_u'] is not None:
+                print("Warning: both eps_u and epsilon are given, ignoring the eps_u.")
+            options['eps_s'] = epsilon
+            options['eps_u'] = epsilon
+        if options['eps_s'] is None:
+            options['eps_s'] = self.find_epsilon(options['eps_guess_s'], rfp_s, vector_s, -1)
+        if options['eps_u'] is None:
+            options['eps_u'] = self.find_epsilon(options['eps_guess_u'], rfp_u, vector_u)
+
+        # Compute the unstable starting configuration and the manifold 
+        RZs = self.start_config(options['eps_u'], rfp_u, vector_u, lambda_u, options['neps'])[0]
         if 'u+' in options['directions']:
             print("Computing unstable manifold with postive epsilon...")
             self.unstable['+'] = self.integrate(RZs, nintersect=options['nintersect'])
 
         if 'u-' in options['directions']:
-            RZs  = 2*rz_fixedpoint - RZs
+            RZs  = 2*rfp_u - RZs
             print("Computing unstable manifold with negative epsilon...")
             self.unstable['-'] = self.integrate(RZs, nintersect=options['nintersect'])
         
-        RZs = self.start_config(epsilon, self.vector_s, self.lambda_s, options['neps'], -1)[0]
+        # Compute the stable starting configuration and the manifold 
+        RZs = self.start_config(options['eps_s'], rfp_s, vector_s, lambda_s, options['neps'], -1)[0]
         if 's+' in options['directions']:
             print("Computing stable manifold with positive epsilon...")
             self.stable['+'] = self.integrate(RZs, nintersect=options['nintersect'], direction=-1)
         
         if 's-' in options['directions']:
             print("Computing stable manifold with negative epsilon...")
-            RZs = 2*rz_fixedpoint - RZs
+            RZs = 2*rfp_s - RZs
             self.stable['-'] = self.integrate(RZs, nintersect=options['nintersect'], direction=-1)
 
 
-    def start_config(self, epsilon, eigenvector, eigenvalue, neps=10, direction=1, intervtype = "logspace"):
-        rfp = np.array([self.fixedpoint.x[0], self.fixedpoint.z[0]])
+    def start_config(self, epsilon, rfp, eigenvector, eigenvalue = None, neps=10, direction=1, intervtype = "logspace"):
         rEps = rfp + epsilon * eigenvector
-        rz_path = self.integrate(np.atleast_2d(rEps), 1, direction)
+        rz_path = self.integrate(rEps, 1, direction)
         
         eps_dir = rz_path[:,1]-rz_path[:,0]
         norm_eps_dir = np.linalg.norm(eps_dir)
@@ -110,8 +242,8 @@ class Manifold(BaseSolver):
         return RZs, np.linalg.norm(eps_dir_norm - eigenvector)
     
 
-    def find_epsilon(self, eps_guess, eigenvector, iter = 4):
-        find_eps = lambda x: self.start_config(x, eigenvector, 1)[1]
+    def find_epsilon(self, eps_guess, rfp, eigenvector, direction = 1, iter = 4):
+        find_eps = lambda x: self.start_config(x, rfp, eigenvector, neps = 1, direction = direction)[1]
 
         # eps_root = fsolve(find_eps, eps_guess, xtol=options['xtol'])
         try:
@@ -124,6 +256,7 @@ class Manifold(BaseSolver):
         return eps_root
 
     def integrate(self, RZstart, nintersect, direction = 1):
+        RZstart = np.atleast_2d(RZstart)
         rz_path = np.zeros((2*RZstart.shape[0], nintersect+1))
         rz_path[:,0] = RZstart.flatten()
         
@@ -149,6 +282,31 @@ class Manifold(BaseSolver):
                 rz_path[2*i+1, j+1] = output[1]
 
         return rz_path
+    
+    def integrate_single(self, RZstart, nintersect, direction = 1, ret_jacobian = True):
+        r, z = RZstart
+        t0 = self._params["zeta"]
+        dt = direction*2*np.pi/self.bfield.Nfp
+
+        t = t0
+        if ret_jacobian:
+            ic = np.array([r, z, 1.0, 0.0, 0.0, 1.0], dtype=np.float64)
+            self._integrator.change_rhs(self.bfield.f_RZ_tangent)
+        else:
+            ic = np.array([r, z], dtype=np.float64)
+
+        self._integrator.set_initial_value(t, ic)
+        for _ in range(nintersect):
+            output = self._integrator.integrate(t + dt)
+            t = t + dt
+
+        if ret_jacobian:    
+            self._integrator.change_rhs(self.bfield.f_RZ)
+            jacobian = output[2:].reshape((2, 2)).T
+            return output[:2], jacobian
+        else:
+            return output[:2]
+
 
     def plot(self, ax = None, directions = "u+u-s+s-", color = None, end = None, **kwargs):
         default = {"markersize": 2, "fmt": "-o", "colors": ['red', 'blue', 'green', 'purple']}
