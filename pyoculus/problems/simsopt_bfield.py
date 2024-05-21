331223d451144538b31e5d07a00b17664102f899
Akitzu <ludovic.rais@epfl.ch>
2024-05-21 08:48:21 +0200

    interpolatedfield in simsopt problem

--- pyoculus/problems/simsopt_bfield.py
+++ pyoculus/problems/simsopt_bfield.py
@@ -1,16 +1,100 @@
 from pyoculus.problems import CartesianBfield
-from simsopt.field import MagneticField
+from simsopt.field import MagneticField, InterpolatedField, BiotSavart
+from simsopt.geo import SurfaceXYZFourier, SurfaceClassifier
 import numpy as np
+from typing import Union
+
+
+def surf_from_coils(coils, **kwargs):
+    print(kwargs)
+    mpol = kwargs.get('mpol', 3)
+    ntor = kwargs.get('ntor', 3)
+    stellsym = kwargs.get('stellsym', False)
+    nfp = kwargs.get('nfp', 1)
+
+    ncoils = kwargs.get('ncoils', None)
+    
+    nphi, ntheta = len(coils), len(coils[0].curve.gamma())
+    qpts_theta = np.linspace(0, 1, ntheta, endpoint=False)
+    qpts_phi = np.linspace(0, 1, nphi, endpoint=False)
+
+    surf = SurfaceXYZFourier(
+        mpol=mpol,
+        ntor=ntor,
+        stellsym=stellsym,
+        nfp=nfp,
+        quadpoints_phi=qpts_phi,
+        quadpoints_theta=qpts_theta
+    )
+    centroids = np.array([np.mean(coil.curve.gamma(), axis=0) for coil in coils])
+
+    phis = np.arctan2(centroids[:, 1], centroids[:, 0])
+    indices = np.argsort(phis)
+    gamma_curves = [coils[i].curve.gamma() for i in indices]
+    if ncoils is not None:
+        gamma_curves = np.stack([gamma if (i // ncoils) % 2 != 0 else gamma[::-1] for i, gamma in enumerate(gamma_curves)])
+    surf.least_squares_fit(gamma_curves)
+
+    return surf
+
+
 
 ### Simsopt magnetic field problem class ###
 class SimsoptBfieldProblem(CartesianBfield):
-    def __init__(self, R0, Z0, Nfp, bs):
+    def __init__(self, R0, Z0, Nfp, mf, interpolate: Union[bool, InterpolatedField] = False, **kwargs):
         super().__init__(R0, Z0, Nfp)
 
-        if not isinstance(bs, MagneticField):
-            raise ValueError("bs must be a MagneticField object")
+        if not isinstance(mf, MagneticField):
+            raise ValueError("mf must be a MagneticField object")
 
-        self.bs = bs
+        self._mf = mf
+
+        if interpolate:
+            self.interpolating = True
+            if isinstance(interpolate, InterpolatedField):
+                self._mf_B = interpolate
+            else:
+                surf = kwargs.get('surf', surf_from_coils(mf.coils, **kwargs))
+                
+                p = kwargs.get('p', 2)
+                h = kwargs.get('h', 0.03)
+                surfclassifier = SurfaceClassifier(surf, h=h, p=p)
+
+                deltah = kwargs.get('deltah', 0.05)
+                def skip(rs, phis, zs):
+                    rphiz = np.asarray([rs, phis, zs]).T.copy()
+                    dists = surfclassifier.evaluate_rphiz(rphiz)
+                    skip = list((dists < -deltah).flatten())
+                    return skip
+
+                n = kwargs.get('n', 20)
+                rs = np.linalg.norm(surf.gamma()[:, :, 0:2], axis=2)
+                zs = surf.gamma()[:, :, 2]
+                rrange = (np.min(rs), np.max(rs), n)
+                phirange = (0, 2 * np.pi / Nfp, n * 2)
+                zrange = (0, np.max(zs), n // 2)
+
+                degree = kwargs.get('degree', 3)
+                stellsym = kwargs.get('stellsym', True)
+                self._mf_B = InterpolatedField(
+                    mf,
+                    degree,
+                    rrange,
+                    phirange,
+                    zrange,
+                    True,
+                    nfp=Nfp,
+                    stellsym=stellsym,
+                    skip=skip,
+                )
+        else:
+            self.interpolating = False
+            self._mf_B = mf
+
+    @classmethod
+    def from_coils(cls, R0, Z0, Nfp, coils, **kwargs):
+        mf = BiotSavart(coils)
+        return cls(R0, Z0, Nfp, mf, **kwargs)
 
     # The return of the B field for the two following methods is not the same as the calls are :
     #   - CartesianBfield.f_RZ which does :
@@ -21,12 +105,14 @@ class SimsoptBfieldProblem(CartesianBfield):
     # and both should result in a (3,1) array
     def B(self, xyz):
         xyz = np.reshape(xyz, (-1, 3))
-        self.bs.set_points(xyz)
-        return self.bs.B().flatten()
+        self._mf_B.set_points(xyz)
+        return self._mf_B.B().flatten()
 
     def dBdX(self, xyz):
-        B = self.B(xyz)
-        return [B], self.bs.dB_by_dX().reshape(3, 3)
+        xyz = np.reshape(xyz, (-1, 3))
+        self._mf.set_points(xyz)
+        
+        return [self._mf.B().flatten()], self._mf.dB_by_dX().reshape(3, 3)
 
     def B_many(self, x1arr, x2arr, x3arr, input1D=True):
         if input1D:
@@ -38,15 +124,15 @@ class SimsoptBfieldProblem(CartesianBfield):
             ).T
 
         xyz = np.ascontiguousarray(xyz, dtype=np.float64)
-        self.bs.set_points(xyz)
+        self._mf_B.set_points(xyz)
 
-        return self.bs.B()
+        return self._mf_B.B()
 
     def dBdX_many(self, x1arr, x2arr, x3arr, input1D=True):
         B = self.B_many(x1arr, x2arr, x3arr, input1D=input1D)
-        return [B], self.bs.dB_by_dX()
+        return [B], self._mf.dB_by_dX()
     
     def A(self, xyz):
         xyz = np.reshape(xyz, (-1, 3))
-        self.bs.set_points(xyz)
-        return self.bs.A().flatten()
+        self._mf.set_points(xyz)
+        return self._mf.A().flatten()
