8470ff5de20093d357e21a46f7aaef0b22592d68
Akitzu <ludovic.rais@epfl.ch>
2024-06-25 14:26:03 +0200

    autodoc : using Google napoleon type

--- doc/conf.py
+++ doc/conf.py
@@ -18,7 +18,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(".."))
 
-extensions = ['myst_parser', 'sphinx.ext.autodoc']
+extensions = ['myst_parser', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
