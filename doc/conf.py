ced433786cf8276c9d42b43e426232a7aa5a9ad8
Akitzu <ludovic.rais@epfl.ch>
2024-06-25 15:02:18 +0200

    proper requirement dependencies

--- doc/conf.py
+++ doc/conf.py
@@ -14,10 +14,6 @@ release = '0.3.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('..'))
-
 extensions = ['myst_parser', 'sphinx.ext.autodoc', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
