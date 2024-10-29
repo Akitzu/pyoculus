f23591a990fe112cbf3e6e7a6d15c069156ff9e3
Chris <christopher.smiet@epfl.ch>
2024-10-29 14:54:56 +0100

    catch jax-imports in init

--- pyoculus/maps/__init__.py
+++ pyoculus/maps/__init__.py
@@ -2,14 +2,22 @@
 This submodule provides the maps.
 """
 
+
 from .base_map import BaseMap
 from .integrated_map import IntegratedMap
+import logging
+logger = logging.getLogger(__name__)
 
 # from .standard_map import StandardMap
 
 from .toroidal_bfield_section import ToroidalBfieldSection
 from .cylindrical_bfield_section import CylindricalBfieldSection
-
-from .tokamap import TokaMap
-from .standard_map import StandardMap
+try: 
+    from .tokamap import TokaMap
+except ImportError as e:
+    logger.debug(e)
+try: 
+    from .standard_map import StandardMap
+except ImportError as e:
+    logger.debug(e)
 # from .spec_pjh import *
